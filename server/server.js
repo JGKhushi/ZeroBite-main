718afa406cb7d925d309fe7fa5a6285a559db1c1
KHUSHI NEMA <119919728+JGKhushi@users.noreply.github.com>
2025-10-28 18:36:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,12 +6,18 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://zerobite-main-frontend.onrender.com",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static('uploads'));
 
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
